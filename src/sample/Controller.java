f5b48b23a452fde6b444a21e62f732f7e3c4e3ab
chitranshu651 <chitranshu651@gmail.com>
2018-08-28 22:23:10 +0000

    teacher Signup page created
    Lacks functionality but has the required fields

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.sql.*;
+import java.io.IOException;
+
+
+
 public class Controller {
+    public void Tsignup(ActionEvent click) throws IOException {
+        Parent TeacherSign= FXMLLoader.load(getClass().getResource("Teacher_SignUp.fxml"));
+        Scene TeacherSignupScene= new Scene(TeacherSign);
+
+        Stage window = (Stage)((Node) click.getSource()).getScene().getWindow();
+        window.setScene(TeacherSignupScene);
+        window.show();
+
+
+    }
+
 }
---
+++ src/sample/Teacher_SignUp_Controller.java
@@ -0,0 +1,5 @@
+package sample;
+
+public class Teacher_SignUp_Controller {
+
+}
